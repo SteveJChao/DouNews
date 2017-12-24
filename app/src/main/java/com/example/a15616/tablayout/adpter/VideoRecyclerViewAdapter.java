714ae9b85c2b5e73abf7399177c97322c4bb44cb
hungryao <stevechao0324@gmail.com>
2017-12-24 14:04:22 +0800

    Refactor the VideoViewPage, add replace VideoView with JZVideoPlayer

--- app/src/main/java/com/example/a15616/tablayout/adpter/VideoRecyclerViewAdapter.java
+++ app/src/main/java/com/example/a15616/tablayout/adpter/VideoRecyclerViewAdapter.java
@@ -4,7 +4,12 @@ import android.Manifest;
 import android.app.Activity;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
+import android.media.ThumbnailUtils;
 import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
@@ -21,21 +26,19 @@ import com.example.a15616.tablayout.R;
 import com.example.a15616.tablayout.activity.MainActivity;
 
 import java.io.File;
+import java.util.Random;
+
+import cn.jzvd.JZVideoPlayerStandard;
 
 /**
  * Created by hungryao on 12/22/2017.
  */
 
-public class VideoRecyclerViewAdapter extends RecyclerView.Adapter <VideoRecyclerViewAdapter.Holder> implements View.OnClickListener{
+public class VideoRecyclerViewAdapter extends RecyclerView.Adapter <VideoRecyclerViewAdapter.Holder> {
 
     //后文Toast使用
     private Context mContext;
     private Activity mActivity;
-
-    private VideoView videoView;
-    private ImageButton butReplay;
-    private ImageButton butPlay;
-    private ImageButton butStop;
 
     public VideoRecyclerViewAdapter(Context mContext, Activity mActivity) {
         this.mContext = mContext;
@@ -54,24 +57,6 @@ public class VideoRecyclerViewAdapter extends RecyclerView.Adapter <VideoRecycle
                 .from(parent.getContext())
                 .inflate(R.layout.video_item, parent, false);
 
-        videoView = (VideoView) view.findViewById(R.id.video_view);
-        butReplay = (ImageButton) view.findViewById(R.id.replay);
-        butPlay = (ImageButton) view.findViewById(R.id.play);
-        butStop = (ImageButton) view.findViewById(R.id.stop);
-
-        butReplay.setOnClickListener(this);
-        butPlay.setOnClickListener(this);
-        butStop.setOnClickListener(this);
-
-        //运行时权限的处理
-        if (ContextCompat.checkSelfPermission(mContext,
-                Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(mActivity, new String[] {
-                    Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
-        } else {
-            initVideoPath();
-        }
-
         return new Holder(view);
     }
 
@@ -82,13 +67,22 @@ public class VideoRecyclerViewAdapter extends RecyclerView.Adapter <VideoRecycle
      */
     @Override
     public void onBindViewHolder(Holder holder, int position) {
-        final View mView = holder.mView;
-        mView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(mContext, "This is video item on RecyclerView", Toast.LENGTH_SHORT).show();
-            }
-        });
+//        if (holder.file != null) {
+//            holder.videoView.seekTo(getRandom());
+//        }
+        final View mView = holder.mView;//from line 57
+    }
+
+    /**
+     * 产生随机时间
+     * @return
+     */
+    public int getRandom() {
+        int max=150;
+        int min=20;
+        Random random = new Random();
+        int s = random.nextInt(max)%(max-min+1) + min;
+        return s * 1000;
     }
 
     @Override
@@ -96,73 +90,88 @@ public class VideoRecyclerViewAdapter extends RecyclerView.Adapter <VideoRecycle
         return 5;
     }
 
-    @Override
-    public void onClick(View view) {
-        switch (view.getId()) {
-            case R.id.replay:
-                if (videoView.isPlaying()) {
-                    videoView.resume();
-                }
-                break;
-            case R.id.play:
-                if (!videoView.isPlaying()) {
-                    videoView.start();
-                }
-                break;
-            case R.id.stop:
-                if (videoView.isPlaying()) {
-                    videoView.pause();
-                }
-                break;
-        }
-    }
 
-
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
-                                           @NonNull int[] grantResults) {
-        switch (requestCode) {
-            case 1:
-                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    initVideoPath();
-                } else {
-                    Toast.makeText(mContext, "You have denied the permission.", Toast.LENGTH_SHORT).show();
-                }
-                break;
-            default:
-        }
-    }
-
-    /**
-     * 初始化播放器
-     */
-    private void initVideoPath() {
-        File file = new File(Environment.getExternalStorageDirectory(), "qqmusic/mv/movie.mp4");
-        if (file == null) {
-            Toast.makeText(mContext, "The video does not exists.", Toast.LENGTH_SHORT).show();
-            mActivity.finish();
-        } else {
-            videoView.setVideoPath(file.getPath());
-        }
-    }
-
-
-    public class Holder extends RecyclerView.ViewHolder {
+    class Holder extends RecyclerView.ViewHolder implements View.OnClickListener{
         private View mView;
+
+        protected File file;
+        private ImageButton butReplay;
+        private ImageButton butPlay;
+        private ImageButton butStop;
+
         public Holder(View itemView) {
             super(itemView);
             this.mView = itemView;
+
+            JZVideoPlayerStandard jzVideoPlayerStandard = (JZVideoPlayerStandard) mView.findViewById(R.id.video_player);
+            jzVideoPlayerStandard.setUp("http://jzvd.nathen.cn/c6e3dc12a1154626b3476d9bf3bd7266/6b56c5f0dc31428083757a45764763b0-5287d2089db37e62345123a1be272f8b.mp4"
+                    , JZVideoPlayerStandard.SCREEN_WINDOW_NORMAL, "Demo tile");
+            jzVideoPlayerStandard.thumbImageView.setImageResource(R.drawable.poster);
+            butReplay = (ImageButton) itemView.findViewById(R.id.replay);
+            butPlay = (ImageButton) itemView.findViewById(R.id.play);
+            butStop = (ImageButton) itemView.findViewById(R.id.stop);
+
+            butReplay.setOnClickListener(this);
+            butPlay.setOnClickListener(this);
+            butStop.setOnClickListener(this);
+
+            //运行时权限的处理
+            if (ContextCompat.checkSelfPermission(mContext,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(mActivity, new String[] {
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+            } else {
+//                initVideoPath();
+            }
+
+        }
+
+        /**
+         * 初始化播放器
+         */
+//        private void initVideoPath() {
+//            file = new File(Environment.getExternalStorageDirectory(), "qqmusic/mv/movie.mp4");
+//            if (file == null) {
+//                Toast.makeText(mContext, "The video does not exists.", Toast.LENGTH_SHORT).show();
+//            } else {
+//                videoView.setVideoPath(file.getPath());
+//            }
+//        }
+
+        @Override
+        public void onClick(View view) {
+//            switch (view.getId()) {
+//                case R.id.replay:
+//                    if (videoView.isPlaying()) {
+//                        videoView.resume();
+//                    }
+//                    break;
+//                case R.id.play:
+//                    if (!videoView.isPlaying()) {
+//                        videoView.start();
+//                    }
+//                    break;
+//                case R.id.stop:
+//                    if (videoView.isPlaying()) {
+//                        videoView.pause();
+//                    }
+//                    break;
+//            }
+        }
+
+        public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                               @NonNull int[] grantResults) {
+            switch (requestCode) {
+                case 1:
+                    if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+//                        initVideoPath();
+                    } else {
+                        Toast.makeText(mContext, "You have denied the permission.", Toast.LENGTH_SHORT).show();
+                    }
+                    break;
+                default:
+            }
         }
     }
 
-    /**
-     * view离开window时停止播放
-     * @param holder
-     */
-    @Override
-    public void onViewAttachedToWindow(Holder holder) {
-        super.onViewAttachedToWindow(holder);
-        if (videoView != null) {
-            videoView.suspend();
-        }
-    }
 }
