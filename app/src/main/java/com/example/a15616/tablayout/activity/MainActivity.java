e8bb8eb65ab28892783289a23a84ddb16678f3d4
SteveJChao <stevechao0324@gmail.com>
2018-01-21 21:16:44 +0800

    Add drawer

--- app/src/main/java/com/example/a15616/tablayout/activity/MainActivity.java
+++ app/src/main/java/com/example/a15616/tablayout/activity/MainActivity.java
@@ -3,17 +3,22 @@ package com.example.a15616.tablayout.activity;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 import com.example.a15616.tablayout.adpter.MyFragmentStatePagerAdapter;
 import com.example.a15616.tablayout.fragment.ListFragment;
@@ -29,8 +34,6 @@ public class MainActivity extends AppCompatActivity {
 
     private ViewPager mViewPager;
 
-    private ImageButton startMenu;
-
     private DrawerLayout mDrawerLayout;
 
 
@@ -41,18 +44,39 @@ public class MainActivity extends AppCompatActivity {
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.tool_bar);
         setSupportActionBar(toolbar);
+        final ActionBar actionBar = getSupportActionBar();
+        actionBar.setHomeAsUpIndicator(R.drawable.menu_16);
+        actionBar.setDisplayHomeAsUpEnabled(true);
 
         mViewPager = (ViewPager) findViewById(R.id.view_pager);
-        startMenu = (ImageButton) findViewById(R.id.menu);
         mDrawerLayout = (DrawerLayout) findViewById(R.id.main_drawer);
-        initViewPager();
+        NavigationView navigationView = (NavigationView) findViewById(R.id.main_navigation_view);
+        if (navigationView != null) {
+            navigationView.setNavigationItemSelectedListener(
+                    new NavigationView.OnNavigationItemSelectedListener() {
+                        @Override
+                        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                            item.setChecked(true);
+                            String title = item.getTitle().toString();
+                            Toast.makeText(getApplicationContext(), title, Toast.LENGTH_SHORT).show();
+                            mDrawerLayout.closeDrawers();
+                            return true;
+                        }
+                    }
+            );
+        }
 
-        startMenu.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+        initViewPager();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
                 mDrawerLayout.openDrawer(GravityCompat.START);
-            }
-        });
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     public void initViewPager() {
